9e9110a891e749af55063a7dc234588f73c83149
levinhphuc95 <levinhphuc95@gmail.com>
2021-05-24 23:51:10 +0700

    Prepare Netlify deploy

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "https://git.heroku.com/peaceful-lowlands-51657.git";
+    : "https://git.heroku.com/peaceful-lowlands-51657.git/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "JustOne";
 
