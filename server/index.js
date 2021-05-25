2fd0d67ea1ebd0ed8f9c9c4f70d31379fb7e9272
levinhphuc95 <levinhphuc95@gmail.com>
2021-05-25 10:42:08 +0700

    Fix Cors

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://elegant-hermann-20f82b.netlify.app" }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
